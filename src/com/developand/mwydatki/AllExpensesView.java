69b640d1bc3b27ada05427be2775920605afc3d5
Andrei Karpushonak <karpushonak@gmail.com>
2011-03-19 16:16:59 +0100

    downloader separated to class

--- src/com/developand/mwydatki/AllExpensesView.java
+++ src/com/developand/mwydatki/AllExpensesView.java
@@ -1,6 +1,5 @@
 package com.developand.mwydatki;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
-import com.developand.mwydatki.data.DataReader;
-import com.developand.mwydatki.data.DataReaderImpl;
+import com.developand.mwydatki.concurrent.data.DataDownloader;
 import com.developand.mwydatki.data.OperationEntry;
 import com.developand.mwydatki.data.OperationEntryAdapter;
 import com.developand.mwydatki.data.common.OperationType;
@@ -20,70 +18,33 @@ import com.developand.mwydatki.tools.ToastMaker;
 
 public class AllExpensesView extends ListActivity {
 
-	private static final String TAG = "SumaListView";
+	private static final String TAG = AllExpensesView.class.getName();
 
-	private ProgressDialog progressDialog = null;
+	private ProgressDialog progressDialog;
 	private List<OperationEntry> operations = null;
 	private OperationEntryAdapter opEntryAdapter;
-	private Runnable dataReader;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		operations = new ArrayList<OperationEntry>();
-		this.opEntryAdapter = new OperationEntryAdapter(this, R.layout.detailed_view,
-				operations, this);
+		this.opEntryAdapter = new OperationEntryAdapter(this,
+				R.layout.detailed_view, operations, this);
 		setListAdapter(this.opEntryAdapter);
 
-		dataReader = new Runnable() {
+		progressDialog = ProgressDialog.show(AllExpensesView.this, "",
+				this.getString(R.string.converting), true);
 
-			public void run() {
-				downloadData();
-			}
-		};
-		Thread thread = new Thread(null, dataReader, "parser");
+		Thread thread = new Thread(null, new DataDownloader(operations,
+				OperationType.ALL, this, opEntryAdapter, progressDialog),
+				"parser");
 		thread.start();
-
-		String converting = this.getString(R.string.converting);
-		String wait = this.getString(R.string.please_wait);
-		progressDialog = ProgressDialog.show(AllExpensesView.this, wait, converting,
-				true);
 	}
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-		// TODO Auto-generated method stub
-		//super.onListItemClick(l, v, position, id);
 		Log.v(TAG, "position = " + position);
 		Object obj = operations.toArray()[position];
-		ToastMaker.getToast(this, "you selected " + obj);
+		ToastMaker.getToast(this, obj.toString());
 	}
-	
-	private void downloadData() {
-
-		DataReader dr = new DataReaderImpl();
-		try {
-			// no not allow cache - it is first view to show
-			dr.readData(false);
-			operations = dr.getOperationsByIndex(0, OperationType.ALL);
-
-			Log.d(TAG, "size = " + operations.size());
-
-			this.runOnUiThread(new Runnable() {
-				
-				public void run() {
-					opEntryAdapter.clear();
-					if (operations != null && operations.size() > 0) {
-						opEntryAdapter.notifyDataSetChanged();
-						for (int i = 0; i < operations.size(); i++)
-							opEntryAdapter.add(operations.get(i));
-					}
-					progressDialog.dismiss();
-					opEntryAdapter.notifyDataSetChanged();					
-				}
-			});
-		} catch (IOException e) {
-			Log.e(TAG, e.getMessage());
-		}
-	}	
 }
---
+++ src/com/developand/mwydatki/concurrent/data/ActiveObject.java
@@ -0,0 +1,10 @@
+package com.developand.mwydatki.concurrent.data;
+
+public abstract class ActiveObject implements Runnable {
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
