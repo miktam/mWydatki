778ea35769c5ff4609bf46e8219e6c7f93884df2
Andrei Karpushonak <karpushonak@gmail.com>
2011-03-19 16:35:41 +0100

    small packages fixes

---
+++ src/com/developand/mwydatki/data/concurrent/ActiveObject.java
@@ -0,0 +1,10 @@
+package com.developand.mwydatki.data.concurrent;
+
+public abstract class ActiveObject implements Runnable {
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
