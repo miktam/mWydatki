b58c8e860f0d85fa59c1b8d4ce4282deaa471b94
Andrei Karpushonak <karpushonak@gmail.com>
2011-03-24 00:32:15 +0100

    added icons

--- gen/com/developand/mwydatki/R.java
+++ gen/com/developand/mwydatki/R.java
@@ -11,7 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int btn_circle_disable=0x7f020000;
+        public static final int btn_circle_normal=0x7f020001;
+        public static final int btn_square_overlay_disabled=0x7f020002;
+        public static final int btn_square_overlay_normal=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int tab_all=0x7f020005;
+        public static final int tab_detailes=0x7f020006;
     }
     public static final class id {
         public static final int date=0x7f050004;
--- src/com/developand/mwydatki/Main.java
+++ src/com/developand/mwydatki/Main.java
@@ -10,7 +10,7 @@ import android.util.Log;
 import android.widget.TabHost;
 
 public class Main extends TabActivity {
-	private static final String TAG = "Main";
+	private static final String TAG = Main.class.getName();
 
 	/** Called when the activity is first created. */
 
@@ -22,37 +22,30 @@ public class Main extends TabActivity {
 		TabHost tabHost = getTabHost(); // The activity TabHost
 		TabHost.TabSpec spec; // Resusable TabSpec for each tab
 		Intent intent; // Reusable Intent for each tab
-		
+
 		try {
 
-		// Create an Intent to launch an Activity for the tab (to be reused)
-		intent = new Intent().setClass(this, AllExpensesView.class);
+			// Create an Intent to launch an Activity for the tab (to be reused)
+			intent = new Intent().setClass(this, AllExpensesView.class);
 
-		// Initialize a TabSpec for each tab and add it to the TabHost
-		spec = tabHost.newTabSpec("all").setIndicator("All", null)
-				.setContent(intent);
-		tabHost.addTab(spec);
+			// Initialize a TabSpec for each tab and add it to the TabHost
+			spec = tabHost
+					.newTabSpec("all")
+					.setIndicator("Wszystko",
+							res.getDrawable(R.drawable.tab_all))
+					.setContent(intent);
+			tabHost.addTab(spec);
 
-		// Do the same for the other tabs
-		intent = new Intent().setClass(this, IncomeView.class);
-		spec = tabHost.newTabSpec("plus").setIndicator("Dochody", null)
-				.setContent(intent);
-		tabHost.addTab(spec);
+			intent = new Intent().setClass(this, DetailedBillsView.class);
+			spec = tabHost
+					.newTabSpec("minus")
+					.setIndicator("Pogrupowane",
+							res.getDrawable(R.drawable.tab_detailes))
+					.setContent(intent);
+			tabHost.addTab(spec);
 
-		intent = new Intent().setClass(this, BillsView.class);
-		spec = tabHost.newTabSpec("minus").setIndicator("Wydatki", null)
-				.setContent(intent);
-		tabHost.addTab(spec);
-		
-		intent = new Intent().setClass(this, DetailedBillsView.class);
-		spec = tabHost.newTabSpec("minus").setIndicator("Details", null)
-				.setContent(intent);
-		tabHost.addTab(spec);
-			
-		tabHost.setCurrentTab(0);
-		}
-		catch (Exception e)
-		{
+			tabHost.setCurrentTab(0);
+		} catch (Exception e) {
 			Log.w(TAG, "really bad: " + e.getMessage());
 			ToastMaker.getToast(this, "File is corrupted?");
 			this.finish();
