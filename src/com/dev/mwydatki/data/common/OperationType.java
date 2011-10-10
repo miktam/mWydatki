4eb84c07d4f0df30681291ad5b766e79ad3b6fc4
Andrei Karpushonak <karpushonak@gmail.com>
2011-10-10 21:25:36 +0200

    version 2.0

--- src/com/dev/mwydatki/data/common/OperationType.java
+++
@@ -1,6 +0,0 @@
-package com.dev.mwydatki.data.common;
-
-public enum OperationType {
-	ALL, PLUS, MINUS, DETAILED_MINUS, DETAILED_PLUS
-
-}
--- src/com/dev/mwydatki/data/concurrent/ActiveObject.java
+++
@@ -1,8 +0,0 @@
-package com.dev.mwydatki.data.concurrent;
-
-public abstract class ActiveObject implements Runnable {
-
-	public void run() {
-	}
-
-}
